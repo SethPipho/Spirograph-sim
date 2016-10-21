278fb0d61fba30e17e0c71707592defb28e2e2b3
SethPipho <seth.pipho@gmail.com>
2016-10-20 21:11:33 -0500

    smoother line with shadowblur

--- js/spirograph.js
+++ js/spirograph.js
@@ -95,7 +95,7 @@ function loop()
 
 		        drawingCtx.lineWidth = 1;
 				drawingCtx.lineCap = "round"
-                drawingCtx.shadowBlur = 2
+                drawingCtx.shadowBlur = 1.5
                 drawingCtx.shadowColor = "rgb(0,0,0)"
 		        drawingCtx.stroke()
 
